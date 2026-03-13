48519e83512f78ebcf12ecb0e216c63612cc2d7c
AditiSharma-2026 <bhoomisharma2652006@gmail.com>
2026-03-13 23:02:49 +0530

    Add Person class with constructor and display method

---
+++ Person.java
@@ -0,0 +1,26 @@
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    Person(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    // Method to display details
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        // Creating two Person objects
+        Person p1 = new Person("Aditi", 17);
+        Person p2 = new Person("Rahul", 20);
+
+        // Printing details
+        p1.display();
+        p2.display();
+    }
+}
